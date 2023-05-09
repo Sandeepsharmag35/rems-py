4f6106b90adfe1aa3cb6d4ce0b6ef7ba4acb5698
Sandeepsharmag35 <72307548+Sandeepsharmag35@users.noreply.github.com>
2023-05-09 08:16:58 +0545

    Updated files

--- rems/rems/settings.py
+++ rems/rems/settings.py
@@ -101,7 +101,7 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
     {
-        'NAME': 'login.validators.SpecialCharacterAndNumberValidator',
+        'NAME': 'accounts.validators.SpecialCharacterAndNumberValidator',
     },
 
 ]
