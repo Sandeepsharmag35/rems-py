6615ea8506b072cc77719ff6c0ddc9de40921083
Sandeepsharmag35 <72307548+Sandeepsharmag35@users.noreply.github.com>
2023-05-15 08:31:38 +0545

    Updated Files

--- rems/admin_panel/urls.py
+++ rems/admin_panel/urls.py
@@ -5,7 +5,5 @@ urlpatterns = [
     path("admin/", views.adminLogin, name="admin_login"),
     path("admin-logout/", views.adminLogout, name="admin_logout"),
     path("dashboard/", views.AdminDashboard, name="admin_dashboard"),
-
-
-
+    path("messages/", views.customerMessage, name="messages"),
 ]
--- rems/admin_panel/views.py
+++ rems/admin_panel/views.py
@@ -2,21 +2,25 @@ from django.shortcuts import render, redirect, HttpResponse
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
+from app.models import CustomerMessage
 
 # Create your views here.
 
+
 def adminLogin(request):
-    if request.method == 'POST':
-        username = request.POST.get('admin-username')
-        password = request.POST.get('admin-password')
+    if request.method == "POST":
+        username = request.POST.get("admin-username")
+        password = request.POST.get("admin-password")
         user = authenticate(request, username=username, password=password)
         if user is not None and user.is_superuser:
             login(request, user)
-            return redirect('admin_dashboard')
+            return redirect("admin_dashboard")
         else:
             error_message = "Your username or password is incorrect."
-            return render(request, 'admin/admin-login.html', {'error_message': error_message})
-    return render(request, 'admin/admin-login.html')
+            return render(
+                request, "admin/admin-login.html", {"error_message": error_message}
+            )
+    return render(request, "admin/admin-login.html")
 
 
 def adminLogout(request):
@@ -26,4 +30,9 @@ def adminLogout(request):
 
 @login_required(login_url=login)
 def AdminDashboard(request):
-    return render(request, "admin/dashboard.html", {'user': request.user}) 
+    return render(request, "admin/dashboard.html", {"user": request.user})
+
+
+def customerMessage(request):
+    message = CustomerMessage.objects.all()
+    return render(request, "admin/message.html", {"message": message})
