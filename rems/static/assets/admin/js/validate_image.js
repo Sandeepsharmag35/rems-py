f29a56b9f69be341ae411e897d7c051ff6d99c8c
Sandeepsharmag35 <72307548+Sandeepsharmag35@users.noreply.github.com>
2023-09-15 18:08:06 +0545

    Updated code

--- rems/static/assets/admin/js/validate_image.js
+++ rems/static/assets/admin/js/validate_image.js
@@ -1,56 +1,58 @@
-const PropertyFrontPicInput = document.getElementById('front-image');
-const PropertyFrontPicPreview = document.getElementById('front-pic');
+document.addEventListener('DOMContentLoaded', function () {
+    const PropertyFrontPicInput = document.getElementById('front-image');
+    const PropertyFrontPicPreview = document.getElementById('front-pic');
 
-const PropertySidePicInput = document.getElementById('side-image');
-const PropertySidePicPreview = document.getElementById('side-pic');
+    const PropertySidePicInput = document.getElementById('side-image');
+    const PropertySidePicPreview = document.getElementById('side-pic');
 
-const PropertyExtraPicInput = document.getElementById('extra-image');
-const PropertyExtraPicPreview = document.getElementById('extra-pic');
+    const PropertyExtraPicInput = document.getElementById('extra-image');
+    const PropertyExtraPicPreview = document.getElementById('extra-pic');
 
-const PropertyExtra2PicInput = document.getElementById('extra2-image');
-const PropertyExtra2PicPreview = document.getElementById('extra2-pic');
+    const PropertyExtra2PicInput = document.getElementById('extra2-image');
+    const PropertyExtra2PicPreview = document.getElementById('extra2-pic');
 
-PropertyFrontPicInput.addEventListener('change', function (event) {
-    const file = event.target.files[0];
-    const reader = new FileReader();
+    PropertyFrontPicInput.addEventListener('change', function (event) {
+        const file = event.target.files[0];
+        const reader = new FileReader();
 
-    reader.onload = function (e) {
-        PropertyFrontPicPreview.src = e.target.result;
-    };
+        reader.onload = function (e) {
+            PropertyFrontPicPreview.src = e.target.result;
+        };
 
-    reader.readAsDataURL(file);
+        reader.readAsDataURL(file);
+    });
+
+    PropertySidePicInput.addEventListener('change', function (event) {
+        const file = event.target.files[0];
+        const reader = new FileReader();
+
+        reader.onload = function (e) {
+            PropertySidePicPreview.src = e.target.result;
+        };
+
+        reader.readAsDataURL(file);
+    });
+
+    PropertyExtraPicInput.addEventListener('change', function (event) {
+        const file = event.target.files[0];
+        const reader = new FileReader();
+
+        reader.onload = function (e) {
+            PropertyExtraPicPreview.src = e.target.result;
+        };
+
+        reader.readAsDataURL(file);
+    });
+
+    PropertyExtra2PicInput.addEventListener('change', function (event) {
+        const file = event.target.files[0];
+        const reader = new FileReader();
+
+        reader.onload = function (e) {
+            PropertyExtra2PicPreview.src = e.target.result;
+        };
+
+        reader.readAsDataURL(file);
+    });
+
 });
-
-PropertySidePicInput.addEventListener('change', function (event) {
-    const file = event.target.files[0];
-    const reader = new FileReader();
-
-    reader.onload = function (e) {
-        PropertySidePicPreview.src = e.target.result;
-    };
-
-    reader.readAsDataURL(file);
-});
-
-PropertyExtraPicInput.addEventListener('change', function (event) {
-    const file = event.target.files[0];
-    const reader = new FileReader();
-
-    reader.onload = function (e) {
-        PropertyExtraPicPreview.src = e.target.result;
-    };
-
-    reader.readAsDataURL(file);
-});
-
-PropertyExtra2PicInput.addEventListener('change', function (event) {
-    const file = event.target.files[0];
-    const reader = new FileReader();
-
-    reader.onload = function (e) {
-        PropertyExtra2PicPreview.src = e.target.result;
-    };
-
-    reader.readAsDataURL(file);
-});
-
