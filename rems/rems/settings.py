13032c2825b0acdcaf1cb7df364f46d92d90c70b
Sandeepsharmag35 <72307548+Sandeepsharmag35@users.noreply.github.com>
2024-04-10 14:16:35 +0545

    Updated

--- rems/rems/settings.py
+++ rems/rems/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
+
 from pathlib import Path
 import os
 from decouple import config
@@ -22,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-a!*^(xv(c85-4h*yqu+ua#_!c$&46&lmv7f@y&26j!(thw6cts"
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
